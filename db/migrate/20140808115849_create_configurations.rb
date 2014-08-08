ab77cecc8ae308878f3330901f4ceee802848a8e
Sohaib Talaat Bhatti <sohaibbbhatti@gmail.com>
2014-08-08 17:57:24 +0500

    configurations model

---
+++ db/migrate/20140808115849_create_configurations.rb
@@ -0,0 +1,12 @@
+class CreateConfigurations < ActiveRecord::Migration
+  def change
+    create_table :configurations do |t|
+      t.string :key, null: false
+      t.text :value
+
+      t.timestamps
+    end
+
+    add_index :configurations, :key
+  end
+end
